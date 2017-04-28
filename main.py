eced62527e6a6d2ab3f861d55965ae7815c33814
giedroyc <rgiedroyc@gmail.com>
2017-04-28 22:34:37 +0100

    pass arg to endpoint

--- main.py
+++ main.py
@@ -13,5 +13,11 @@ async def data(request):
     import datasources.data
     return html(datasources.data.data.compute().to_html())
 
+@app.route("/data/<i:int>")
+async def data(request, i):
+    import datasources.data
+    df = datasources.data.data.compute()
+    return json(df.loc[i].to_json())
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
