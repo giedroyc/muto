77f455edd2dd5016af5b2ca9b3c3363979914203
giedroyc <rgiedroyc@gmail.com>
2017-04-28 21:37:19 +0100

    simple data response

---
+++ datasources/data.py
@@ -0,0 +1,3 @@
+import dask.dataframe as dd
+
+data = dd.read_csv('sample/data.csv')
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from sanic import Sanic
 
-from sanic.response import json
+from sanic.response import json, html
 
 app = Sanic()
 
 @app.route("/")
 async def test(request):
     return json({"hello": "world"})
+
+@app.route("/data")
+async def data(request):
+    import datasources.data
+    return html(datasources.data.data.compute().to_html())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
